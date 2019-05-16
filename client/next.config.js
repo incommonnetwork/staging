03080564b938a93f614f19244470d5ecec316c6a
Ryan Bennett <ry.nomad@protonmail.com>
2019-05-15 19:16:23 -0600

    update static paths

--- client/next.config.js
+++ client/next.config.js
@@ -1,9 +1,26 @@
+
+const webpack = require('webpack')
+
 const withSass = require('@zeit/next-sass')
 const withTM = require('next-transpile-modules');
+
+
+const isStaging = (process.env.NODE_ENV) === 'staging'
+const assetPrefix = isStaging ? '/staging' : ''
 
 module.exports = withSass(withTM({
     transpileModules: ['react-bulma-components'],
     sassLoaderOptions: {
         includePaths: ["./client"]
+    },
+    assetPrefix,
+    webpack: config => {
+        config.plugins.push(
+            new webpack.DefinePlugin({
+                'process.env.ASSET_PREFIX': JSON.stringify(assetPrefix),
+            }),
+        )
+
+        return config
     },
 }))
