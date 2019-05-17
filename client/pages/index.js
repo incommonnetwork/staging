981e66344e51f4cabe4db79b253a3e14d80f2748
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-17 06:07:06 +0000

    Deploy incommonnetwork/staging to github.com/incommonnetwork/staging.git:gh-pages

--- client/pages/index.js
+++ client/pages/index.js
@@ -3,7 +3,6 @@ import Main from '../layouts/main';
 
 export default () => (
   <Main>
-    Hello Travis
     <iframe id="spark" width="100%" height="100%" frameBorder="0" src="https://spark.adobe.com/page/oFoZUUzRZITY1/" />
   </Main>
 )
