8df3d542a0ac3cc072602f4a1b20a18515e5f3ee
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-21 02:50:20 +0000

    Deploy incommonnetwork/staging to github.com/incommonnetwork/staging.git:gh-pages

---
+++ config/test.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "host": "localhost",
+  "port": 3030,
+  "public": "../public/",
+  "paginate": {
+    "default": 10,
+    "max": 50
+  },
+  "postgres": "postgres://incommon:incommon@localhost:5432/incommon"
+}
