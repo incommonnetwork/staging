0cb0e41d959d02e0ec5667a78833d23c6831bac9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-21 00:37:48 +0000

    Deploy incommonnetwork/staging to github.com/incommonnetwork/staging.git:gh-pages

--- server/middleware/next.js
+++ server/middleware/next.js
@@ -6,7 +6,7 @@ const nextPages = new Set(jetpack.cwd(path.join(__dirname, '..','..','client','p
 nextPages.add('');
 
 module.exports = function() {
-  return function next(req, res, next) {
-    return ((req.path.indexOf('/_next') === 0) || nextPages.has(req.path.split('/').pop())) ? handle(req, res) : next();
-  };
+    return function next(req, res, next) {
+        return ((req.path.indexOf('/_next') === 0) || nextPages.has(req.path.split('/').pop())) ? handle(req, res) : next();
+    };
 };
