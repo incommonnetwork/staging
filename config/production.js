e8d89a11e2224af806a3dbc0da4aa5cb4518f71e
Ryan Bennett <ry.nomad@protonmail.com>
2019-05-22 21:35:31 -0600

    add production and staging environments

---
+++ config/production.js
@@ -0,0 +1,5 @@
+module.exports = {
+    "host": "api.incommon.dev",
+    "port": "4040",
+    "postgres": process.env.POSTGRES_CONNECTION
+}
---
+++ config/staging.js
@@ -0,0 +1,5 @@
+module.exports = {
+    "host": "staging.incommon.dev",
+    "port": "4040",
+    "postgres": process.env.POSTGRES_CONNECTION
+}
