e3ca3e63090605b4566b4ed0c99750a49fb40c91
Ryan Bennett <ry.nomad@protonmail.com>
2019-05-24 11:12:25 -0600

    working with stagetest

--- digitalocean/deploy.sh
+++ digitalocean/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "ansible-playbook ./api/playbook.yml --extra-vars \"host=$1 incommon_NODE_ENV=$2 incommon_POSTGRES_CONNECTION=$3\""
+ansible-playbook ./api/playbook.yml --extra-vars "incommon_host=$1 incommon_NODE_ENV=$2 incommon_POSTGRES_CONNECTION=$3"
