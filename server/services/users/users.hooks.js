499a10267aac5ddd4839aa37b80af8e663e2ab62
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-25 05:23:26 +0000

    Deploy incommonnetwork/staging to github.com/incommonnetwork/staging.git:gh-pages

--- server/services/users/users.hooks.js
+++ server/services/users/users.hooks.js
@@ -1,22 +1,46 @@
 const { authenticate } = require('@feathersjs/authentication').hooks;
+const { Forbidden, NotImplemented } = require('@feathersjs/errors');
 
 const {
     hashPassword, protect
 } = require('@feathersjs/authentication-local').hooks;
 
+const authorize = (context) => {
+    const { user, query } = context.params;
+
+    /* eslint-disable no-fallthrough */
+    switch (context.method) {
+        case 'find':
+            if (user) throw new Forbidden('Not Authorized');
+        case 'get':
+        case 'update':
+        case 'patch':
+        case 'remove':
+            if (user && (user.id !== query.id)) throw new Forbidden('Not Authorized');
+            break;
+        default:
+            throw new NotImplemented(__dirname);
+
+    }
+    /* eslint-enable no-fallthrough */
+
+    return context;
+};
+
+
 module.exports = {
     before: {
         all: [],
-        find: [ authenticate('jwt') ],
-        get: [ authenticate('jwt') ],
-        create: [ hashPassword() ],
-        update: [ hashPassword(),  authenticate('jwt') ],
-        patch: [ hashPassword(),  authenticate('jwt') ],
-        remove: [ authenticate('jwt') ]
+        find: [authenticate('jwt'), authorize],
+        get: [authenticate('jwt'), authorize],
+        create: [hashPassword()],
+        update: [hashPassword(), authenticate('jwt'), authorize],
+        patch: [hashPassword(), authenticate('jwt'), authorize],
+        remove: [authenticate('jwt'), authorize]
     },
 
     after: {
-        all: [ 
+        all: [
             // Make sure the password field is never sent to the client
             // Always must be the last hook
             protect('password')
