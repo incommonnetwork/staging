0acc683a778d2a77270348bd930c61730fd3cb2e
Ryan Bennett <ry.nomad@protonmail.com>
2019-05-27 18:43:44 -0600

    propagate children properly to link

--- client/components/link.js
+++ client/components/link.js
@@ -7,12 +7,15 @@ import NextJsLink from 'next/link';
 
 const assetPrefix = process.env.ASSET_PREFIX;
 
-const Link = ({ href }) => (
-    <NextJsLink href={href} as={`${assetPrefix}${href}`} />
+const Link = ({ href, children }) => (
+    <NextJsLink href={href} as={`${assetPrefix}${href}`}>
+        {children}
+    </NextJsLink>
 );
 
 Link.propTypes = {
-    href: PropTypes.string.isRequired
+    href: PropTypes.string.isRequired,
+    children: PropTypes.node
 };
 
 export default Link;
