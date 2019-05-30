3ef49926ef3b629b64a62a41fb9ac006a4452e21
Ryan Bennett <ry.nomad@protonmail.com>
2019-05-30 15:12:44 -0600

    getting to ci seeding

--- migrations/config.js
+++ migrations/config.js
@@ -2,7 +2,7 @@ const app = require('../server/app');
 const env = process.env.NODE_ENV || 'development';
 const dialect = 'postgres';
 
-console.log('DIALECT', dialect)
+console.log('DIALECT', dialect, app.get(dialect))
 
 module.exports = {
     [env]: {
