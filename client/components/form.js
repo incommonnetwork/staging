8dfccf513d2334c1b4c595d9fa0dfdb9842c5255
Ryan Bennett <ry.nomad@protonmail.com>
2019-05-31 20:42:21 -0600

    get form by id

--- client/components/form.js
+++ client/components/form.js
@@ -5,27 +5,30 @@ import 'bootstrap/dist/css/bootstrap.css';
 import JsonSchemaForm from 'react-jsonschema-form';
 import Button from 'react-bulma-components/src/components/button';
 
-const Form = ({ current: { value, meta: { SignUp: { schema, uiSchema, validate, onSubmit } } }, send }) => (
-    <JsonSchemaForm
-        disabled={value === 'form_submit'}
-        schema={schema}
-        uiSchema={uiSchema}
-        validate={validate}
-        onSubmit={onSubmit(send)}
-    >
-        <Button
-            className="is-primary"
-            fullwidth
-            loading={value === 'form_submit'}
-            type="submit">
-            Submit
+const Form = ({ id, current: { value, meta: { SignUp: { schema, uiSchema, validate, onSubmit } } }, send }) => (
+    <div id={id + '_form'}>
+        <JsonSchemaForm
+            disabled={value === 'form_submit'}
+            schema={schema}
+            uiSchema={uiSchema}
+            validate={validate}
+            onSubmit={onSubmit(send)}
+        >
+            <Button
+                className="is-primary"
+                fullwidth
+                loading={value === 'form_submit'}
+                type="submit">
+                Submit
         </Button>
-    </JsonSchemaForm>
+        </JsonSchemaForm>
+    </div>
 );
 
 Form.propTypes = {
     current: PropTypes.object.isRequired,
-    send: PropTypes.func.isRequired
+    send: PropTypes.func.isRequired,
+    id: PropTypes.string.isRequired
 };
 
 export default Form;
--- client/pages/sign_up.js
+++ client/pages/sign_up.js
@@ -18,7 +18,7 @@ const SignUp = () => {
 
     return (
         <CardLayout>
-            <Element current={current} send={send} />
+            <Element current={current} send={send} id={'sign_up'} />
         </CardLayout>
     );
 };
