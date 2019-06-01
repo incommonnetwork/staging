c926906d8e4a729183363f9a6f4e3bfa8d310d8b
Ryan Bennett <ry.nomad@protonmail.com>
2019-05-31 20:22:04 -0600

    sign up integration test init

---
+++ test/client/sign_up.test.js
@@ -0,0 +1,33 @@
+/* global jasmine */
+const puppeteer = require('puppeteer');
+const env = require('../setup.env')(3032);
+const getPage = env.getPage;
+
+describe('/', () => {
+    beforeAll(async () => {
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+        this.browser = await puppeteer.launch();
+        await env.before();
+    });
+
+    afterAll(async () => {
+        await env.after();
+        await this.browser.close();
+    });
+
+    beforeEach(async () => {
+        this.page = await this.browser.newPage();
+        await this.page.goto(getPage('/sign_up'));
+    });
+
+    afterEach(async () => {
+        await this.page.close();
+    });
+
+    it('loads', async () => {
+        expect.assertions(1);
+        const content = await this.page.content();
+        expect(content.indexOf('<!DOCTYPE html')).toBe(0);
+    });
+
+});
