f9f4a123ff41d3a0affbe67647521cd3fdd3f0fa
Ryan Bennett <ry.nomad@protonmail.com>
2019-05-31 22:58:20 -0600

    point to correct api

--- client/utils/feathers.js
+++ client/utils/feathers.js
@@ -4,10 +4,18 @@ import rest from '@feathersjs/rest-client';
 
 let app = null;
 
+const getClientApi = (app) => {
+    const host = app.get('host')
+    const port = app.get('port')
+    const protocol = app.get('protocol')
+
+    return `${protocol}://${host}${protocol === 'https' ? '' : port}`
+}
+
 async function init(req) {
     const app = feathers();
     const _fetch = req ? require('node-fetch') : fetch; // eslint-disable-line no-undef
-    const api = req ? `http://localhost${process.env.NODE_ENV === 'production' ? '' : ':3030'}` : undefined;
+    const api = req ? `http://localhost${process.env.NODE_ENV === 'production' ? '' : ':3030'}` : getClientApi(app);
     const restClient = rest(api);
     app.configure(restClient.fetch(_fetch));
     const authConfig = { storage: req ? undefined : localStorage }; // eslint-disable-line no-undef
