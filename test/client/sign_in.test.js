d671074adb26fb2c953db2d5df136a044a9c10b9
Ryan Bennett <ry.nomad@protonmail.com>
2019-07-06 20:09:16 -0600

    headless chrome, d'oh

--- test/client/sign_in.test.js
+++ test/client/sign_in.test.js
@@ -6,7 +6,7 @@ const { getPage, getPathname } = env;
 
 describe('/sign_in', () => {
     beforeAll(async () => {
-        this.browser = await puppeteer.launch({ headless: false });
+        this.browser = await puppeteer.launch({});
         await env.before();
     });
 
