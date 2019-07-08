db0b666427f46239dce9bd765b28050d2bb7317b
Ryan Bennett <ry.nomad@protonmail.com>
2019-07-08 00:36:23 -0600

    run admin tabs test for each

--- test/client/admin.test.js
+++ test/client/admin.test.js
@@ -1,10 +1,10 @@
 /* global location */
-const puppeteer = require('puppeteer');
-const env = require('../setup.env')(6032);
+const puppeteer = require(`puppeteer`);
+const env = require(`../setup.env`)(6032);
 const { getPage, getPathname } = env;
 
 
-describe('/admin', () => {
+describe(`/admin`, () => {
     beforeAll(async () => {
         this.browser = await puppeteer.launch({});
         await env.before();
@@ -19,7 +19,7 @@ describe('/admin', () => {
         this.context = await this.browser.createIncognitoBrowserContext();
         this.app = await env.initApi();
         this.page = await this.context.newPage();
-        await this.page.goto(getPage('/admin'));
+        await this.page.goto(getPage(`/admin`));
     });
 
     afterEach(async () => {
@@ -28,20 +28,20 @@ describe('/admin', () => {
         this.app = null;
     });
 
-    it('loads', async () => {
+    it(`loads`, async () => {
         expect.assertions(1);
         const content = await this.page.content();
-        expect(content.indexOf('<!DOCTYPE html')).toBe(0);
+        expect(content.indexOf(`<!DOCTYPE html`)).toBe(0);
     });
 
-    it('redirects to sign in', async () => {
-        await this.page.waitFor((pathname) => pathname === location.pathname, { timeout: 60000 }, getPathname('/sign_in'));
+    it(`redirects to sign in`, async () => {
+        await this.page.waitFor((pathname) => pathname === location.pathname, { timeout: 60000 }, getPathname(`/sign_in`));
     });
 
-    it('redirects to home if not admin', async () => {
+    it(`redirects to home if not admin`, async () => {
 
-        await this.page.waitFor((pathname) => pathname === location.pathname, { timeout: 60000 }, getPathname('/sign_in'));
-        const form = await this.page.$('#sign_in_form');
+        await this.page.waitFor((pathname) => pathname === location.pathname, { timeout: 60000 }, getPathname(`/sign_in`));
+        const form = await this.page.$(`#sign_in_form`);
         const rand = `${Math.random()}`;
         const good_input = {
             email: `${rand}@test.com`,
@@ -49,79 +49,83 @@ describe('/admin', () => {
             confirm_password: rand,
         };
 
-        await this.app.service('users').create(good_input);
+        await this.app.service(`users`).create(good_input);
 
-        await this.page.type('#root_email', good_input.email);
-        await this.page.type('#root_password', good_input.password);
+        await this.page.type(`#root_email`, good_input.email);
+        await this.page.type(`#root_password`, good_input.password);
 
-        const submit_button = await form.$('button.is-primary');
+        const submit_button = await form.$(`button.is-primary`);
         await submit_button.click();
 
-        await this.page.waitFor((pathname) => pathname === location.pathname, { timeout: 5000 }, getPathname('/home'));
+        await this.page.waitFor((pathname) => pathname === location.pathname, { timeout: 5000 }, getPathname(`/home`));
     });
 
-    describe('tabs', () => {
+    describe(`tabs`, () => {
         beforeEach(async () => {
-            await this.page.waitFor((pathname) => pathname === location.pathname, { timeout: 5000 }, getPathname('/sign_in'));
-            const form = await this.page.$('#sign_in_form');
+            await this.page.waitFor((pathname) => pathname === location.pathname, { timeout: 5000 }, getPathname(`/sign_in`));
+            const form = await this.page.$(`#sign_in_form`);
             const good_input = {
-                email: 'admin@mock.admin',
-                password: 'admin123',
+                email: `admin@mock.admin`,
+                password: `admin123`,
             };
 
-            await this.page.type('#root_email', good_input.email);
-            await this.page.type('#root_password', good_input.password);
+            await this.page.type(`#root_email`, good_input.email);
+            await this.page.type(`#root_password`, good_input.password);
 
-            const submit_button = await form.$('button.is-primary');
+            const submit_button = await form.$(`button.is-primary`);
             await submit_button.click();
 
-            await this.page.waitFor((pathname) => pathname === location.pathname, { timeout: 5000 }, getPathname('/admin'));
+            await this.page.waitFor((pathname) => pathname === location.pathname, { timeout: 5000 }, getPathname(`/admin`));
         });
 
-        it('has tabs', async () => {
-            await this.page.waitFor('#admin_tabs', {});
+        it(`has tabs`, async () => {
+            await this.page.waitFor(`#admin_tabs`, {});
         });
 
-        describe('users', async () => {
-            beforeEach(async () => {
-                const tab = await this.page.waitFor('#users_tab');
-                await tab.click();
+        for (const id of [`users`, `codes`, `cities`, `neighborhoods`]) {
+            describe(id, async () => {
+                beforeEach(async () => {
+                    const tab = await this.page.waitFor(`#${id}_tab`);
+                    await tab.click();
+                });
+
+                it(`has tab`, async () => {
+                    await this.page.waitFor(`#${id}_tab`, {});
+                });
+
+                it(`has tab content`, async () => {
+                    await this.page.waitFor(`#${id}_tab_content`, {});
+                });
+
+                it(`has table`, async () => {
+                    await this.page.waitFor(`#${id}_table`);
+                });
+
+                it(`has filter dropdown`, async () => {
+                    await this.page.waitFor(`#${id}_filter_dropdown`);
+                });
+
+                it(`can filter based on id`, async () => {
+                    expect.assertions(1);
+                    const dropdown = await this.page.waitFor(`#${id}_filter_dropdown`);
+                    await dropdown.click();
+                    const id_selector = await this.page.waitFor(`#${id}_id_selector`);
+                    await id_selector.click();
+                    await this.page.type(`#${id}_filter`, `1`);
+                    const submit_button = await this.page.waitFor(`#${id}_filter_submit`);
+                    await submit_button.click();
+                    await this.page.waitFor(`#${id}_table_body`);
+                    const total = await this.page.$eval(`#${id}_table_body`, (node) => node.getAttribute(`total`));
+
+                    expect(total).toBe(`1`);
+                });
+
+
+
             });
-
-            it('has tab', async () => {
-                await this.page.waitFor('#users_tab', {});
-            });
-
-            it('has tab content', async () => {
-                await this.page.waitFor('#users_tab_content', {});
-            });
-
-            it('has table', async () => {
-                await this.page.waitFor('#users_table');
-            });
-
-            it('has filter dropdown', async () => {
-                await this.page.waitFor('#users_filter_dropdown');
-            });
-
-            it('can filter based on id', async () => {
-                expect.assertions(1);
-                const dropdown = await this.page.waitFor('#users_filter_dropdown');
-                await dropdown.click();
-                const id_selector = await this.page.waitFor('#users_id_selector');
-                await id_selector.click();
-                await this.page.type('#users_filter', '1');
-                const submit_button = await this.page.waitFor('#users_filter_submit');
-                await submit_button.click();
-                await this.page.waitFor('#users_table_body');
-                const total = await this.page.$eval('#users_table_body', (node) => node.getAttribute('total'));
-
-                expect(total).toBe('1');
-            });
+        }
 
 
-
-        });
     });
 
 });
