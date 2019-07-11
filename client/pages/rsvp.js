02d5e01cae9727545c289e1b2b7fea4482cf8658
Ryan Bennett <ry.nomad@protonmail.com>
2019-07-10 23:33:29 -0600

    add rsvp page

---
+++ client/pages/rsvp.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import Protected from '../layouts/protected';
+import { NarrowColumn } from '../layouts/columns';
+import Card from '../layouts/card';
+
+import Form from '../components/form';
+import formContext from '../schemas/rsvp_form';
+
+const context = {
+    ...formContext,
+    submit_service_done: () => {
+        // console.log('done');
+    }
+};
+
+const Rsvp = () => {
+
+
+    return (
+        <Protected>
+            <NarrowColumn>
+                <Card>
+                    <Form context={context} id={'rsvp'} />
+                </Card>
+            </NarrowColumn>
+        </Protected>
+    );
+};
+
+export default Rsvp;
