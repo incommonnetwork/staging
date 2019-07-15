d5d699085703fde37a2db39309efbb35b33d47eb
Ryan Bennett <ry.nomad@protonmail.com>
2019-07-15 00:31:12 -0600

    test update

--- test/integrations/end2end.test.js
+++ test/integrations/end2end.test.js
@@ -105,11 +105,8 @@ if (!isProdStaging) {
             }
             this.context = await this.browser.createIncognitoBrowserContext();
             this.page = await this.context.newPage();
-            if (process.env.TEST_ENV === 'staging') {
-                //need to optimize, but sparse trees are generated here
-                this.page.setDefaultTimtout(300000);
-            }
-            //this.page.setDefaultTimeout(3000);
+            // ridiculous timeout, must fix, but testing adds artifacts that are non realistic
+            this.page.setDefaultTimeout(3000000);
         });
 
         afterEach(async () => {
--- test/services/registrations.test.js
+++ test/services/registrations.test.js
@@ -1,6 +1,7 @@
 const env = require('../setup.env.js')(4531);
+const moment = require('moment')
 
-describe('\'invites\' service', () => {
+describe('registration service', () => {
     beforeAll(env.before);
     afterAll(env.after);
 
@@ -21,6 +22,54 @@ describe('\'invites\' service', () => {
 
         it('allows find', async () => {
             await this.api.service('registrations').find();
+        });
+    });
+
+    describe('regstration', () => {
+        beforeEach(async () => {
+            this.dates = [moment().add(1, 'days').format('YYYY[/]MM[/]D')];
+            this.city = await this.api.service('city').create({
+                city: this.rand,
+                state: this.rand,
+                country: this.rand,
+                latitude: 10,
+                longitude: 10
+            });
+
+            this.neighborhood = await this.api.service('neighborhoods').create({
+                cityId: this.city.id,
+                neighborhood: this.rand,
+                latitude: 10,
+                longitude: 10
+            });
+
+            this.code = await this.api.service('codes').create({
+                text: this.rand,
+                description: this.rand,
+                cityId: this.city.id,
+                dates: this.dates
+            });
+
+            this.user = await this.api.service('users').create({
+                email: `${this.rand}@registrations.com`,
+                password: this.rand
+            });
+
+            await this.api.authenticate({
+                strategy: 'local',
+                email: `${this.rand}@registrations.com`,
+                password: this.rand
+            });
+        });
+
+        it('allows user to register', async () => {
+            const registration = await this.api.service('registrations').create({
+                neighborhoodId: this.neighborhood.id,
+                codeId: this.code.id,
+                dates: this.dates[0]
+            });
+
+            expect(registration.userId).toBe(this.user.id);
         });
     });
 
