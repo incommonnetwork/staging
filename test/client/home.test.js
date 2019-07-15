3409b8209f7936518fecad5df16627452fb33d0b
Ryan Bennett <ry.nomad@protonmail.com>
2019-07-14 18:53:27 -0600

    test fixing

--- test/client/home.test.js
+++ test/client/home.test.js
@@ -37,46 +37,4 @@ describe('/home', () => {
     it('redirects to sign in', async () => {
         await this.page.waitFor((pathname) => pathname === location.pathname, { timeout: 60000 }, getPathname('/sign_in'));
     });
-
-    describe('tabs', () => {
-        beforeEach(async () => {
-            await this.page.waitFor((pathname) => pathname === location.pathname, { timeout: 60000 }, getPathname('/sign_in'));
-            const form = await this.page.waitFor('#sign_in_form');
-            const rand = `${Math.random()}`;
-            const good_input = {
-                email: `${rand}@test.com`,
-                password: rand,
-                confirm_password: rand,
-            };
-
-            await this.app.service('users').create(good_input);
-
-            await this.page.type('#root_email', good_input.email);
-            await this.page.type('#root_password', good_input.password);
-
-            const submit_button = await form.$('button.is-primary');
-            await submit_button.click();
-
-            await this.page.waitFor((pathname) => pathname === location.pathname, { timeout: 60000 }, getPathname('/home'));
-        });
-
-        it('has tabs', async () => {
-            await this.page.waitFor('#home_tabs', {});
-        });
-
-        describe('settings', async () => {
-            it('has tab', async () => {
-                await this.page.waitFor('#settings_tab', {});
-            });
-
-            it('has tab content', async () => {
-                await this.page.waitFor('#settings_tab_content', {});
-            });
-
-            it('has form', async () => {
-                await this.page.waitFor('#settings_form');
-            });
-        });
-    });
-
 });
