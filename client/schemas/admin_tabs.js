38aeadcaf2800b758ea988cd4101949391e05872
Ryan Bennett <ry.nomad@protonmail.com>
2019-07-24 13:51:30 -0600

    admin loads OK

--- client/schemas/admin_tabs.js
+++ client/schemas/admin_tabs.js
@@ -1,9 +1,6 @@
 import userTable from './user_table';
 import codeTable from './code_table';
 import cityTable from './city_table';
-import neighborhoodTable from './neighborhood_table';
-import registrationTable from './registration_table';
-import inviteTable from './invite_table';
 import restaurantTable from './restaurant_table';
 import rsvpTable from './rsvps_table';
 import reservationTable from './reservation_table';
@@ -12,9 +9,6 @@ export default [
     userTable,
     codeTable,
     cityTable,
-    neighborhoodTable,
-    registrationTable,
-    inviteTable,
     restaurantTable,
     rsvpTable,
     reservationTable
