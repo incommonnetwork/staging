0260aa13f1c641da863788ad78162a95f1baf719
Ryan Bennett <ry.nomad@protonmail.com>
2019-07-24 20:12:23 -0600

    css for sidekick

--- client/layouts/hero.js
+++ client/layouts/hero.js
@@ -4,17 +4,16 @@ import PropTypes from 'prop-types';
 import BulmaHero from 'react-bulma-components/src/components/hero';
 import Container from 'react-bulma-components/src/components/container';
 import Tile from 'react-bulma-components/src/components/tile';
+import Image from 'react-bulma-components/src/components/image';
 
 import Title from '../components/title';
 
 const Hero = ({ title, subtitle, children }) => (
-    <BulmaHero size="fullheight" color="primary" gradient>
+    <BulmaHero size="fullheight" color="info" gradient>
         <Tile vertical style={{ justifyContent: 'space-around' }}>
-            <BulmaHero size="medium">
-                <Container fluid>
-                    <Title size={1} title={title} subtitle={subtitle} />
-                </Container>
-            </BulmaHero>
+            <Container fluid>
+                <Title size={1} title={title} subtitle={subtitle} />
+            </Container>
             <Container fluid>
                 {children}
             </Container>
@@ -30,5 +29,37 @@ Hero.propTypes = {
         PropTypes.node
     ])
 };
+
+const SideKickImage = ({ src }) => (
+    <Image style={{ flex: 'auto', alignSelf: 'center' }} size={'square'} src={src} />
+)
+
+SideKickImage.propTypes = {
+    src: PropTypes.string.isRequired
+}
+
+const SideKick = ({ children, image, justify = 'left' }) => (
+    <BulmaHero size="fullheight" color="info" gradient style={{ maxHeight: '100vh' }}>
+        <Tile style={{ maxHeight: 'inherit', overflowY: 'hidden' }}>
+            <Tile >
+                {justify !== 'left' ? <SideKickImage src={image} /> : children}
+            </Tile>
+            <Tile>
+                {justify === 'left' ? <SideKickImage src={image} /> : children}
+            </Tile>
+        </Tile>
+    </BulmaHero>
+);
+
+SideKick.propTypes = {
+    image: PropTypes.string.isRequired,
+    justify: PropTypes.string,
+    children: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes.node
+    ])
+}
+
+export { SideKick };
 
 export default Hero;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,27 +1,30 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
-import Hero from '../layouts/hero';
+import Hero, { SideKick } from '../layouts/hero';
 
 import Tile from 'react-bulma-components/src/components/tile';
 import Title from '../components/title';
 import Spark from '../components/spark';
 
 const Index = () => (
-    <Hero title="InCommon" subtitle="Dinner parties for the Digital Age" >
-        <Tile kind="parent">
-            <Tile kind="child" renderAs="article" notification color="primary">
-                <Title size={3} title="For People" />
-                <Spark url="https://spark.adobe.com/video/NnfAON46L5sTa/embed" />
+    <Fragment>
+        <Hero title="InCommon" subtitle="Dinner parties for the Digital Age" >
+            <Tile kind="parent">
+                <Tile kind="child" renderAs="article" notification color="primary">
+                    <Title size={3} title="For People" />
+                    <Spark url="https://spark.adobe.com/video/NnfAON46L5sTa/embed" />
+                </Tile>
+                <Tile kind="child" renderAs="article" notification color="info">
+                    <Title size={3} title="For Events" />
+                    <Spark url="https://spark.adobe.com/video/gwkQg1WKcpTqW/embed" />
+                </Tile>
+                <Tile kind="child" renderAs="article" notification color="info">
+                    <Title size={3} title="For Venues" />
+                </Tile>
             </Tile>
-            <Tile kind="child" renderAs="article" notification color="info">
-                <Title size={3} title="For Events" />
-                <Spark url="https://spark.adobe.com/video/gwkQg1WKcpTqW/embed" />
-            </Tile>
-            <Tile kind="child" renderAs="article" notification color="info">
-                <Title size={3} title="For Venues" />
-            </Tile>
-        </Tile>
-    </Hero>
+        </Hero>
+        <SideKick />
+    </Fragment>
 );
 
 export default Index;
