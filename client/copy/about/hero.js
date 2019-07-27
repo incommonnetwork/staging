49290695ace45c71a4e3086e4fb99119e6abfa2f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-27 20:33:11 +0000

    Deploy incommonnetwork/staging to github.com/incommonnetwork/staging.git:gh-pages

---
+++ client/copy/about/hero.js
@@ -0,0 +1,8 @@
+const title = 'About InCommon';
+
+const subtitle = 'On a mission to bring you back to the real world';
+
+const image = '/static/refugees-wide.jpg';
+
+
+export { title, subtitle, image };
