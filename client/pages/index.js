268f019817d314f8185220e498ec57a3a11305b1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-04 02:16:17 +0000

    Deploy incommonnetwork/staging to github.com/incommonnetwork/staging.git:gh-pages

--- client/pages/index.js
+++ client/pages/index.js
@@ -4,6 +4,9 @@ import Header from '../components/header';
 import Hero, { SideKick } from '../layouts/hero';
 import copy from '../copy/home';
 import Title from '../components/title';
+import Link from '../components/link';
+import Button from '../components/button';
+import Spark from '../components/spark';
 
 const Index = () => (
     <Fragment>
@@ -15,44 +18,88 @@ const Index = () => (
         <SideKick
             name={'people'}
             image={'/static/table-conversation.jpg'}
-            arrow={true}
+            title="For People"
         >
-
-            <Title
-                title="For New Friends"
-                size={2}
-            />
-            <div style={{ margin: '1rem' }} />
             <Title
                 title="1"
-                size={3}
+                size={5}
                 subtitle="Register your interests in the InCommon app"
             />
             <div style={{ margin: '1rem' }} />
             <Title
                 title="2"
-                size={3}
+                size={5}
                 subtitle="RSVP to meals that fit your schedule and budget"
             />
             <div style={{ margin: '1rem' }} />
             <Title
                 title="3"
-                size={3}
+                size={5}
                 subtitle="Tip your server"
             />
+            <Spark url={'https://spark.adobe.com/video/NnfAON46L5sTa/embed'} />
+            <Link href='/sign_up?for=user'>
+                <Button>
+                    Learn More
+                </Button>
+            </Link>
         </SideKick>
         <SideKick
             name={'events'}
             image={'/static/stage.jpg'}
-            arrow={true}
+            title={'For Communities'}
             justify={'right'}
         >
+            <Title
+                title="1"
+                size={5}
+                subtitle="Generate a unique InCommon interest code"
+            />
+            <Title
+                title="2"
+                size={5}
+                subtitle="Share the code with your people"
+            />
+            <Title
+                title="3"
+                size={5}
+                subtitle="Watch your community grow"
+            />
+            <p style={{ textAlign: 'center' }}>
+                InCommon codes can relate to anything. From exclusive ticketed events to podcast interest groups, and everything in between.
+            </p>
+            <Spark url={'https://spark.adobe.com/video/gwkQg1WKcpTqW/embed'} />
+            <Link href='/sign_up?for=user'>
+                <Button>
+                    Learn More
+                </Button>
+            </Link>
         </SideKick>
         <SideKick
             name={'restaurants'}
             image={'/static/reservation.jpg'}
-            arrow={true}
+            title="For Restaurants"
         >
+            <Title
+                title="1"
+                size={5}
+                subtitle="Notice an open table in your reservation book"
+            />
+            <Title
+                title="2"
+                size={5}
+                subtitle="Generate an invite with the table capacity and time"
+            />
+            <Title
+                title="3"
+                size={5}
+                subtitle="Split the check"
+            />
+            <Link href='/sign_up?for=user'>
+                <Button>
+                    Learn More
+                </Button>
+            </Link>
         </SideKick>
     </Fragment >
 );
