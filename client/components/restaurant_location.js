b76677830de0245950ad89f0ec1fd8a88f996220
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-12 20:29:02 +0000

    Deploy incommonnetwork/staging to github.com/incommonnetwork/staging.git:gh-pages

--- client/components/restaurant_location.js
+++ client/components/restaurant_location.js
@@ -4,10 +4,10 @@ import PropTypes from 'prop-types';
 import Modal from './modal';
 
 const RestaurantLocation = ({ value }) => {
-    const { id, address, city, state, map } = JSON.parse(value);
+    const { id, address, city, map } = JSON.parse(value);
     return (
         <div>
-            {`${address}, ${city}, ${state}`}
+            {`${address}, ${city}`}
             <div style={{ padding: 10 }} />
             <Modal id={id} button="Show Map" naked={true}>
                 <iframe src={map} />
